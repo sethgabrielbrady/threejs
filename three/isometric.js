696f3358607a1c33244f6fe44ead2af95a5a1ea0
Seth Brady <sethgabrielbrady@gmail.com>
2018-05-01 09:21:40 -0400

    added input and fixed camera

--- three/isometric.js
+++ three/isometric.js
@@ -10,14 +10,6 @@
 				container = document.createElement( 'div' );
 				document.body.appendChild( container );
 
-				var info = document.createElement( 'div' );
-				info.style.position = 'absolute';
-				info.style.top = '10px';
-				info.style.width = '100%';
-				info.style.textAlign = 'center';
-				info.innerHTML = '<a href="http://threejs.org" target="_blank" rel="noopener">three.js</a> - orthographic view';
-				container.appendChild( info );
-
 				var aspect = window.innerWidth / window.innerHeight;
 				camera = new THREE.OrthographicCamera( frustumSize * aspect / - 2, frustumSize * aspect / 2, frustumSize / 2, frustumSize / - 2, 1, 2000 );
 				camera.position.y = 400;
@@ -26,31 +18,52 @@
 				scene.background = new THREE.Color( 0xf0f0f0 );
 
 				// Grid
-
-				var gridHelper = new THREE.GridHelper( 1000, 20 );
+				var gridHelper = new THREE.GridHelper(1200, 12);
 				scene.add( gridHelper );
 
 				// Cubes
-
 				var geometry = new THREE.BoxGeometry( 50, 50, 50 );
 				var material = new THREE.MeshLambertMaterial( { color: 0xffffff, overdraw: 0.5 } );
 
-				for ( var i = 0; i < 100; i ++ ) {
+          //add random cubes to populate the grid
+    			// for ( var i = 0; i < 10; i ++ ) {
+          //
+    			// 	var cube = new THREE.Mesh( geometry, material );
+    			// 	cube.scale.y = Math.floor( Math.random() * 2 + 1 );
+    			// 	cube.position.x = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
+    			// 	cube.position.y = ( cube.scale.y * 50 ) / 2;
+    			// 	cube.position.z = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
+    			// 	scene.add( cube );
+          //
+    			// }
 
-					var cube = new THREE.Mesh( geometry, material );
-
+					var cube = new THREE.Mesh( geometry, material);
 					cube.scale.y = Math.floor( Math.random() * 2 + 1 );
-
 					cube.position.x = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
 					cube.position.y = ( cube.scale.y * 50 ) / 2;
 					cube.position.z = Math.floor( ( Math.random() * 1000 - 500 ) / 50 ) * 50 + 25;
-
 					scene.add( cube );
 
-				}
+
+          // movement
+          var xSpeed = 50;
+          var zSpeed = 50;
+          document.addEventListener("keydown", onDocumentKeyDown, false);
+          function onDocumentKeyDown(event) {
+            var keyCode = event.which;
+            if (keyCode == 37) {
+                cube.position.z += zSpeed;
+            } else if (keyCode == 39) {
+                cube.position.z -= zSpeed;
+            } else if (keyCode == 40) {
+                cube.position.x += xSpeed;
+            } else if (keyCode == 38) {
+                cube.position.x -= xSpeed;
+            }
+            render();
+          }
 
 				// Lights
-
 				var ambientLight = new THREE.AmbientLight( Math.random() * 0x10 );
 				scene.add( ambientLight );
 
@@ -61,62 +74,41 @@
 				directionalLight.position.normalize();
 				scene.add( directionalLight );
 
-				var directionalLight = new THREE.DirectionalLight( Math.random() * 0xffffff );
-				directionalLight.position.x = Math.random() - 0.5;
-				directionalLight.position.y = Math.random() - 0.5;
-				directionalLight.position.z = Math.random() - 0.5;
-				directionalLight.position.normalize();
-				scene.add( directionalLight );
-
-				renderer = new THREE.CanvasRenderer();
-				renderer.setPixelRatio( window.devicePixelRatio );
-				renderer.setSize( window.innerWidth, window.innerHeight );
+        // renderer
+				renderer = new THREE.WebGLRenderer({canvas: myCanvas});
+        renderer.setClearColor(0x000000);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
 				container.appendChild( renderer.domElement );
 
-				stats = new Stats();
-				container.appendChild( stats.dom );
-
-				//
-
 				window.addEventListener( 'resize', onWindowResize, false );
-
 			}
 
 			function onWindowResize() {
-
 				var aspect = window.innerWidth / window.innerHeight;
-
 				camera.left   = - frustumSize * aspect / 2;
 				camera.right  =   frustumSize * aspect / 2;
 				camera.top    =   frustumSize / 2;
 				camera.bottom = - frustumSize / 2;
-
 				camera.updateProjectionMatrix();
-
 				renderer.setSize( window.innerWidth, window.innerHeight );
-
 			}
-
-			//
 
 			function animate() {
-
 				requestAnimationFrame( animate );
-
-				stats.begin();
 				render();
-				stats.end();
-
 			}
 
+      // set this for fixed camera
+      camera.position.x = Math.cos(100) * 400;
+      camera.position.z = Math.sin(100) * 400;
+
 			function render() {
+        // unset this for rotating camera
+				// var timer = Date.now() * 0.0001;
+				// camera.position.x = Math.cos(timer) * 800;
+				// camera.position.z = Math.sin(timer) * 800;
 
-				var timer = Date.now() * 0.0001;
-
-				camera.position.x = Math.cos( timer ) * 800;
-				camera.position.z = Math.sin( timer ) * 800;
 				camera.lookAt( scene.position );
-
 				renderer.render( scene, camera );
-
 			}
--- three/sprite.js
+++ three/sprite.js
@@ -25,10 +25,10 @@
 
   // var geometry = new THREE.BoxGeometry(100, 100, 100);
 
-  var geometry = new THREE.BoxGeometry(100, 100, 100);
+  var geometry = new THREE.BoxGeometry(500,300, 100);
   var material1 = new THREE.MeshLambertMaterial({color: 0xF3FFE2});
   var mesh1 = new THREE.Mesh(geometry, material1);
-  mesh1.position.set(100, 100, -1000);
+  mesh1.position.set(-100, 100, -1000);
 
 
   // var material = new THREE.MeshLambertMaterial({color: 0xf3ffe2});
@@ -42,21 +42,48 @@
 
 // Sprite material
 var material = new THREE.SpriteMaterial({
-  map: new THREE.TextureLoader().load("brwnbot.png"),
+  map: new THREE.TextureLoader().load("three/brwnbot.png"),
   // color: 0xF3FFE2
 });
 var mesh = new THREE.Sprite(material);
 mesh.scale.set(100, 100, 100);
 mesh.position.z = -1000;
+mesh.position.y = 225;
+mesh.position.x = 225;
 scene.add(mesh, mesh1);
 
+mesh1.rotation.x = 2.25;
+  myCanvas.innerHTML = mesh1.rotation.y;
+  console.log(mesh.position.z, mesh.position.y, mesh.position.x);
+  console.log(mesh1.position.z, mesh1.position.y, mesh1.position.x);
  // var delta = 0;
   //RENDER LOOP
+  var xSpeed = 50;
+  var ySpeed = 50;
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 38) {
+        mesh.position.z += ySpeed;
+    } else if (keyCode == 40) {
+        mesh.position.z -= ySpeed;
+    } else if (keyCode == 39) {
+        mesh.position.x += xSpeed;
+    } else if (keyCode == 37) {
+        mesh.position.x -= xSpeed;
+    }
+        render();
+}
+
   requestAnimationFrame(render);
   function render() {
      // delta += 0.01;
-    material.rotation.x +=  Math.PI / 100;
-    mesh1.rotation.x += 0.025;
+    // material.rotation.x +=  Math.PI / 100;
+    // mesh1.rotation.x += 0.025;
+
+    console.log(mesh.position.z, mesh.position.y, mesh.position.x);
+    console.log(mesh1.position.z, mesh1.position.y, mesh1.position.x);
     renderer.render(scene, camera);
     requestAnimationFrame(render);
   }
